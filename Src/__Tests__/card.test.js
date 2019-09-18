d7d663ac782bffc739605c6d897c97980de49142
Aditya <thebeaditya@gmail.com>
2019-09-18 22:30:02 +0545

    More Tests ❤

--- Src/__Tests__/card.test.js
+++ Src/__Tests__/card.test.js
@@ -4,6 +4,55 @@ const Card = require('../Core/Cards/Card');
 const CardDeck = require('../Core/Cards/CardDeck');
 const CardRule = require('../Core/Cards/CardRule');
 
+describe('CardDeck Tests', () => {
+  describe('Shuffle', () => {
+    const deck = new CardDeck();
+    it('should shuffle', () => {
+      const before = {
+        firstCard: deck.cardsArray[0],
+        lastCard: deck.cardsArray[51],
+      };
+      deck.shuffle();
+      const after = {
+        firstCard: deck.cardsArray[0],
+        lastCard: deck.cardsArray[51],
+      };
+      expect(before.firstCard.value).to.not.equal(after.firstCard.value);
+      expect(before.lastCard.value).to.not.equal(after.lastCard.value);
+    });
+
+    it('should shuffle 10 times', () => {
+      const before = {
+        firstCard: deck.cardsArray[0],
+        lastCard: deck.cardsArray[51],
+      };
+      deck.shuffle(10);
+      const after = {
+        firstCard: deck.cardsArray[0],
+        lastCard: deck.cardsArray[51],
+      };
+      expect(before.firstCard.value).to.not.equal(after.firstCard.value);
+      expect(before.lastCard.value).to.not.equal(after.lastCard.value);
+    });
+
+    it('should throw a RangeError if shuffle is passed with value outside [1,50]', () => {
+      try {
+        deck.shuffle(100);
+      } catch (err) {
+        expect(err.name).to.equal('RangeError');
+      }
+    });
+
+    it('should throw a TypeError if shuffle is passed with value that is not a number', () => {
+      try {
+        deck.shuffle('100');
+      } catch (err) {
+        expect(err.name).to.equal('TypeError');
+      }
+    });
+  });
+});
+
 describe('CardRule Tests', () => {
   describe('Sequence Tests', () => {
     it('should detect A-2-3', () => {
@@ -114,4 +163,6 @@ describe('CardRule Tests', () => {
       expect(result).equal(false);
     });
   });
+
+  describe('Win Tests', () => {});
 });
