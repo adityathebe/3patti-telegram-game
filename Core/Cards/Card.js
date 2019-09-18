6f3d8d02ce4090aaa33437f999943816e4169215
Aditya <thebeaditya@gmail.com>
2019-09-18 17:00:36 +0545

    Added types

--- Core/Cards/Card.js
+++ Core/Cards/Card.js
@@ -1,4 +1,8 @@
 class Card {
+  /**
+   * @param {String} value
+   * @param {"spades" | "diamonds" | "hearts" | "clubs"} suit
+   */
   constructor(value, suit) {
     this.value = value;
     this.suit = suit;
