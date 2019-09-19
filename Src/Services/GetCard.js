fce2d9888d5e2b6d5203141a79e1fd94502c0beb
Aditya <thebeaditya@gmail.com>
2019-09-19 23:22:56 +0545

    BUG FIX

--- Src/Services/GetCard.js
+++ Src/Services/GetCard.js
@@ -3,10 +3,12 @@ const bot = require('../bot');
 const { USERNAME_TG } = require('../config');
 const CardDeck = require('../Core/Cards/CardDeck');
 
-bot.onText(new RegExp('(/cards [2-9]|10)|(/cards)$'), handleGetCard);
-bot.onText(new RegExp(`(/cards@${USERNAME_TG} [2-9]|10)|(\/cards@${USERNAME_TG})$`), handleGetCard);
+bot.onText(new RegExp('^/cards$'), handleGetCard);
+bot.onText(new RegExp('^/cards [2-9]$'), handleGetCard);
+bot.onText(new RegExp(`^/cards@${USERNAME_TG}$`), handleGetCard);
+bot.onText(new RegExp(`^/cards@${USERNAME_TG} [2-9]$`), handleGetCard);
 
-async function handleGetCard(msg, match) {
+async function handleGetCard(msg) {
   const deck = new CardDeck();
   const numPlayers = msg.text.split(' ')[1] || 2;
   const cards = deck.shuffle(10).distribute(numPlayers);
