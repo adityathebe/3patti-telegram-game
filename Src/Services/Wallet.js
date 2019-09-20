9a07d6b0f294fc3840d75a2c8a6ba5fdce6dec02
Aditya <thebeaditya@gmail.com>
2019-09-20 08:26:16 +0545

    Use reply keyboard for wallets

--- Src/Services/Wallet.js
+++ Src/Services/Wallet.js
@@ -9,12 +9,23 @@ bot.onText(/\/wallet|Wallet$/, async msg => {
   if (userInDb === null) {
     return bot.sendMessage(msg.chat.id, 'Please /register first');
   }
-  const replyKeyboardMarkup = {
-    keyboard: [[{ text: 'Add Balance' }], [{ text: 'Withdraw Balance' }], [{ text: 'Transfer Balance' }]],
-    resize_keyboard: true,
-    one_time_keyboard: false,
+  const inlineKeyboardMarkup = {
+    inline_keyboard: [
+      [
+        { text: 'Add Balance', callback_data: 'BALANCE-ADD' },
+        { text: 'Withdraw Balance', callback_data: 'BALANCE-WITHDRAW' },
+      ],
+      [{ text: 'Transfer Balance', callback_data: 'BALANCE-TRANSFER' }],
+    ],
   };
   bot.sendMessage(msg.chat.id, 'The buttons below will not function', {
-    reply_markup: replyKeyboardMarkup,
+    reply_markup: inlineKeyboardMarkup,
   });
+});
+
+bot.on('callback_query', async query => {
+  const payload = query.data;
+  if (payload.indexOf('BALANCE-') !== 0) return;
+  const userChosenOption = payload.replace('BALANCE-', '');
+  bot.answerCallbackQuery(query.id, { text: userChosenOption, cache_time: 60 });
 });
