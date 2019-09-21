b710c35437b32902a708eb8364a281112a2a1db3
Aditya <thebeaditya@gmail.com>
2019-09-21 18:24:38 +0545

    Logger query scripts fixed

--- Src/Logs/query.js
+++ Src/Logs/query.js
@@ -11,10 +11,19 @@ function queryMsgReceived() {
     function(err, results) {
       results.file.forEach(log => {
         if (log.message.telegramMsgReceived) {
-          const messageTxt = log.message.telegramMsgReceived.message.text;
-          const sender = log.message.telegramMsgReceived.message.from.username;
-          const timestamp = new Date(log.timestamp).toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
-          console.log(`[${timestamp}] :: ${sender} ==> ${messageTxt}`);
+          if (log.message.telegramMsgReceived.msgType === 'text') {
+            const messageTxt = log.message.telegramMsgReceived.message.text;
+            const sender = log.message.telegramMsgReceived.message.from.username;
+            const timestamp = new Date(log.timestamp).toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
+            console.log(`[${timestamp}] :: ${sender} ==> ${messageTxt}`);
+          }
+
+          if (log.message.telegramMsgReceived.msgType === 'callback_query') {
+            const messageTxt = log.message.telegramMsgReceived.query.data;
+            const sender = log.message.telegramMsgReceived.query.from.username;
+            const timestamp = new Date(log.timestamp).toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
+            console.log(`[${timestamp}] :: ${sender} ==> ${messageTxt}`);
+          }
         }
       });
     }
@@ -33,10 +42,12 @@ function queryMsgSent() {
       console.log('\n\nMessages sent');
       results.file.forEach(log => {
         if (log.message.telegramMsgSent) {
-          const messageTxt = log.message.telegramMsgSent.text;
-          const sender = log.message.telegramMsgSent.from.username;
-          const timestamp = new Date(log.timestamp).toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
-          console.log(`[${timestamp}] :: ${sender} ==> ${messageTxt}`);
+          if (log.message.telegramMsgSent.text) {
+            const messageTxt = log.message.telegramMsgSent.text;
+            const sender = log.message.telegramMsgSent.from.username;
+            const timestamp = new Date(log.timestamp).toLocaleString('en-US', { timeZone: 'Asia/Kathmandu' });
+            console.log(`[${timestamp}] :: ${sender} ==> ${messageTxt}`);
+          }
         }
       });
     }
