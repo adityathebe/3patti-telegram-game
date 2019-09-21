6b8098267437d042876de392cdfa62db3087bf7a
Aditya <thebeaditya@gmail.com>
2019-09-21 13:32:30 +0545

    Convenient 'Re-draw' button for /cards command

--- Src/Services/GetCard.js
+++ Src/Services/GetCard.js
@@ -3,6 +3,8 @@ const bot = require('../bot');
 const { USERNAME_TG } = require('../config');
 const CardDeck = require('../Core/Cards/CardDeck');
 
+const AppError = require('../Utilities/ErrorHandler');
+
 bot.onText(new RegExp('^/cards$'), handleGetCard);
 bot.onText(new RegExp('^/cards [2-9]$'), handleGetCard);
 bot.onText(new RegExp(`^/cards@${USERNAME_TG}$`), handleGetCard);
@@ -18,5 +20,18 @@ async function handleGetCard(msg) {
     response += `Player ${idx + 1} :\n${handStr}`;
     response += '\n\n';
   });
-  bot.sendMessage(msg.chat.id, response);
+  bot
+    .sendMessage(msg.chat.id, response, {
+      reply_markup: {
+        inline_keyboard: [[{ text: 'Re-draw', callback_data: msg.text }]],
+      },
+    })
+    .catch(AppError.handle);
 }
+
+bot.on('callback_query', async query => {
+  const msg = query.message;
+  msg.text = query.data;
+  bot.answerCallbackQuery(query.id, {}).catch(AppError.handle);
+  handleGetCard(msg);
+});
